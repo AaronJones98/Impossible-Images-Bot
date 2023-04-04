454c3da919c47bc0217dcbb5c20e24796b62d8b9
AaronJones98 <aaron.j1998@outlook.com>
2023-04-04 17:20:15 +0100

    After updates to uploading

--- index.js
+++ index.js
@@ -16,13 +16,45 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
+var image = '';
+var description = '';
+var uploading = false;
+var channel_name = '';
 client.on('messageCreate', (message) => {
+
+    message.channel.messages.fetch({ limit: 2 }).then().then(function(recent_messages){
+        var previous_message = recent_messages.last();
+
+        if(previous_message.content.startsWith('Describe the image')){
+
+            description = message.content;
+            channel_name = message.channel.name;
+
+            //Run the upload
+            message.reply("Image is currently being uploaded to impossibleimages.ai | Please wait for confirmation.")
+    
+            fetch('https://impossibleimages.ai/wp-json/impossibleimages/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `image=${image}&description=${description}&channel=${channel_name}&token=${process.env.TOKEN}`
+            }).then(function(response) {
+                // Do nothing
+            }).then(function(data) {
+                message.reply('Image has been successfully uploaded. Please confirm it can be seen here: https://impossibleimages.ai/images/');
+            }).catch(function(err) {
+                message.reply('There was an error uploading your image');
+            });
+
+        }
+    })
 
     var prompt = message.content;
 
     if(prompt.startsWith("-ii")){
 
-        var image = prompt.split('-ii ')[1]
+        image = prompt.split('-ii ')[1]
         
         if(! prompt.includes('http')){
 
@@ -32,27 +64,9 @@ client.on('messageCreate', (message) => {
         }else{
 
             if(image.includes('.jpg') || image.includes('.png') || image.includes('.gif')){
-    
-                //Run the upload
-                message.reply("Image is currently being uploaded to impossibleimages.ai | Please wait for confirmation.")
 
-                fetch('https://impossibleimages.ai/wp-json/impossibleimages/upload', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: `image=${image}&token=${process.env.TOKEN}`
-                }).then(function(response) {
-                    // Do nothing
-                }).then(function(data) {
-                    message.reply('Image has been successfully uploaded. Please confirm it can be seen here: https://impossibleimages.ai/images/');
-                }).catch(function(err) {
-                    message.reply('There was an error uploading your image');
-                });
-
-            }else{
-
-                message.reply("Wrong image format. Please try uploading a different file type or amend your prompt");
+                uploading = true;
+                message.reply("Describe the image. Just respond to this message.");
 
             }
 
