91516e799188b99b8e060c830c76b149e2e1baa9
AaronJones98 <aaron.j1998@outlook.com>
2024-11-04 20:46:15 +0000

    No description needed

--- index.js
+++ index.js
@@ -16,38 +16,8 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-var image = '';
-var description = '';
 var channel_name = '';
 client.on('messageCreate', (message) => {
-
-    message.channel.messages.fetch({ limit: 3 }).then().then(function(recent_messages){
-        var previous_message = recent_messages.at(1);
-
-        if(previous_message.content.startsWith('Describe the image') && message.channel.id == previous_message.channel.id){
-
-            description = message.content;
-            channel_name = message.channel.name;
-
-            //Run the upload
-            message.reply("Image is currently being uploaded to impossibleimages.ai | Please wait for confirmation.")
-    
-            fetch('https://impossibleimages.ai/wp-json/impossibleimages/upload', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `image=${recent_messages.at(2).content.split('-ii ')[1]}&description=${description}&channel=${channel_name}&token=${process.env.TOKEN}`
-            }).then(function(response) {
-                // Do nothing
-            }).then(function(data) {
-                message.reply('Image has been successfully uploaded. Please confirm it can be seen here: https://impossibleimages.ai/images/');
-            }).catch(function(err) {
-                message.reply('There was an error uploading your image');
-            });
-
-        }
-    })
 
     var prompt = message.content;
 
@@ -59,22 +29,22 @@ client.on('messageCreate', (message) => {
 
         }else{
 
-            image = prompt.split('-ii ')[1]
-            
-            if(! prompt.includes('http')){
+            channel_name = message.channel.name;
 
-                //If user doesn't insert an image
-                message.reply("Missing the image url. Please try again")
-
-            }else{
-
-                if(image.includes('.jpg') || image.includes('.png') || image.includes('.gif')){
-
-                    message.reply("Describe the image. Just respond to this message.");
-
-                }
-
-            }
+            //Run the upload
+            fetch('https://impossibleimages.ai/wp-json/impossibleimages/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `image=${prompt.split('-ii ')[1]}&channel=${channel_name}&token=${process.env.TOKEN}`
+            }).then(function(response) {
+                // Do nothing
+            }).then(function(data) {
+                message.reply("The image has been queued for upload. Please wait atleast 48 hours before processing a reupload.");
+            }).catch(function(err) {
+                message.reply('There was an error uploading your image');
+            });
         
         }
 
